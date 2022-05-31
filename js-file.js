4d9ab6b7eb9d1c8175cf3af0ead9c6911af8714c
John Russell <johnhenryrussell1@gmail.com>
2022-05-31 12:52:41 -0400

    Complete final project

--- js-file.js
+++ js-file.js
@@ -6,7 +6,7 @@ const gridColumnContainer = document.querySelector('.grid-column-container');
 
 // Appends 16 div boxes to one row container 
 function appendRows(rowContainer) {
-for (let i=0; i < 16; i++) {
+for (let i=0; i < 50; i++) {
   const divGrid = document.createElement('div');
   divGrid.classList.add('grid-squares');
   rowContainer.appendChild(divGrid);
@@ -14,14 +14,14 @@ for (let i=0; i < 16; i++) {
 
 // Appends 16 row containers to the main column container
 function createRowContainer() {
-  for (let i=0; i<16; i++) {
+  for (let i=0; i<50; i++) {
     const gridRowContainer = document.createElement('div');
     gridRowContainer.classList.add('grid-row-container');
     gridColumnContainer.appendChild(gridRowContainer);
   };
 };
 
-// Selects all row containers and adds 16 div boxes toe ach
+// Selects all row containers and adds 16 div boxes to each
 function generateGrid() {
   createRowContainer();
   const gridContainer = document.querySelectorAll('.grid-row-container')
